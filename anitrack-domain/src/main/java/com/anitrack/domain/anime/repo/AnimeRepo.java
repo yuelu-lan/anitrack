3fac2d5a65a1a8643a4fdc3d20a4756bd4ddf219
yuelu-lan <942893986@qq.com>
2026-07-06 12:23:48 +0800

    feat: 新增WatchlistItemConverter/WatchlistRepoImpl，AnimeRepo补充listByIds批量查询

--- anitrack-domain/src/main/java/com/anitrack/domain/anime/repo/AnimeRepo.java
+++ anitrack-domain/src/main/java/com/anitrack/domain/anime/repo/AnimeRepo.java
@@ -2,11 +2,15 @@ package com.anitrack.domain.anime.repo;
 
 import com.anitrack.domain.anime.model.Anime;
 
+import java.util.List;
+
 public interface AnimeRepo {
 
     Anime getById(Long id);
 
     Anime getByBangumiId(Long bangumiId);
+
+    List<Anime> listByIds(List<Long> ids);
 
     Anime upsert(Anime anime);
 }
